3e1092e421af6b6237528dcd3e2a9aea7b6e887c
陈炜超 <chenweichao@chenweichaodeMacBook-Pro.local>
2024-03-17 23:24:22 +0800

    feat: init

---
+++ json-rust/target/debug/deps/json_rust-070d00c908d732c2.d
@@ -0,0 +1,5 @@
+/Users/chenweichao/go/src/github.com/secret4233/alfred-workflow/json-rust/target/debug/deps/libjson_rust-070d00c908d732c2.rmeta: src/main.rs
+
+/Users/chenweichao/go/src/github.com/secret4233/alfred-workflow/json-rust/target/debug/deps/json_rust-070d00c908d732c2.d: src/main.rs
+
+src/main.rs:
---
+++ json-rust/target/debug/deps/json_rust-a45d19d4216c8f77.d
@@ -0,0 +1,5 @@
+/Users/chenweichao/go/src/github.com/secret4233/alfred-workflow/json-rust/target/debug/deps/libjson_rust-a45d19d4216c8f77.rmeta: src/main.rs
+
+/Users/chenweichao/go/src/github.com/secret4233/alfred-workflow/json-rust/target/debug/deps/json_rust-a45d19d4216c8f77.d: src/main.rs
+
+src/main.rs:
